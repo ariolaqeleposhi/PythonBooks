cbf40ecf2456ec1b7587341d502553736c7014dd
chantellecasey <chantelle.casey@hotmail.co.uk>
2018-03-24 13:45:10 +0000

    Creating the webpages usig flask

--- Chantelle/Flask Webpages Creation.py
+++ Chantelle/Flask Webpages Creation.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/cfgbooks/')
 def content():
-    return 'Home Page'
+    return render_template('homePage.html')
 
 
 @app.route('/cfgbooks/search/')
 def results():
-    return 'Search Results'
+    return render_template('searchResults.html')
 
 @app.route('/cfgbooks/search/<book_page>/')
 def book(book_page):
-    return book_page
+    return render_template('bookDetails.html')
 
 app.run(debug=True)
